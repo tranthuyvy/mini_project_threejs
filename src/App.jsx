c444861f2ab0c25507812b21198f88df6ce213b0
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-03 12:36:02 +0700

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import StarsCanvas from "./components/canvas/Stars";
+
+
 const App = () => {
 
   return (
-    <div>
-      ttv-threejs
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
