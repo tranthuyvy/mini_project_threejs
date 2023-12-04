9ad2c016b961f4300eaac79d5b95c90c7a0ef5aa
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-04 14:36:55 +0700

    ball tech

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -57,7 +57,7 @@ const Experience = () => {
         <p className={`${styles.sectionSubText} text-center`}>
           What I have done so far
         </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>Project</h2>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Work</h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,13 +14,21 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
+    <Float 
+      speed={1.5} 
+      rotationIntensity={1} 
+      floatIntensity={2}
+    >
+      <ambientLight intensity={0.35} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh 
+        castShadow 
+        receiveShadow
+        scale={3}
+      >
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -40,7 +48,7 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
