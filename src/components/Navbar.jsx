2533514c911d0a80c33920d19bd114664648cff1
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-04 15:19:09 +0700

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
           <img src={logo} alt="Logo" className="w-10 h-10 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             TTV &nbsp; 
-            <span className="sm:block hidden"> | ThreeJs </span>
+            <span className="sm:block hidden"> | Profile </span>
           </p>
         </Link>
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+
+import { SectionWrapper } from "../hoc"
+import { styles } from "../styles"
+import { github } from "../assets"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          My project
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
