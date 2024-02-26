549099271b23e7a6716ecfb782208af06efffb19
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-02-26 22:44:30 +0700

    FixBug technologies

--- src/constants/index.js
+++ src/constants/index.js
@@ -78,14 +78,14 @@ const services = [
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
+  // {
+  //   name: "HTML 5",
+  //   icon: html,
+  // },
+  // {
+  //   name: "CSS 3",
+  //   icon: css,
+  // },
   {
     name: "JavaScript",
     icon: javascript,
@@ -98,42 +98,26 @@ const technologies = [
     name: "Tailwind CSS",
     icon: tailwind,
   },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
   {
     name: "Node JS",
     icon: nodejs,
   },
   {
-    name: "Java",
-    icon: java,
-  },
-  {
-    name: "Springboot",
-    icon: springboot,
-  },
-  {
-    name: "PHP",
-    icon: php,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
     name: "git",
     icon: git,
   },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "jira",
-    icon: jira,
-  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "jira",
+  //   icon: jira,
+  // },
 ];
 
 const careers = [
